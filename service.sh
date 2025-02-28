d99b45873c5e4c9428d3340aa707adb002303d73
IsHacker <ishayu301@gmail.com>
2025-02-28 14:47:31 +0530

    v3: Create service.sh

---
+++ service.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+#Wait for boot completion
+sleep 20
+# Turn on "Disable HW Overlays" (not working)
+service call SurfaceFlinger 1008 i32 1
+# Disable Low Memory Killer (LMK)
+chmod 666 /sys/module/lowmemorykiller/parameters/minfree
+chown root /sys/module/lowmemorykiller/parameters/minfree
+echo '0,0,0,0,0,0' > /sys/module/lowmemorykiller/parameters/minfree
+#Try to simulate the behavior of MTK engineering mode, which force enables VoLTE (not working; use MTK Engineering mode instead)
+setprop persist.radio.vilte_ut_support 1
+setprop persist.ims.simulate 1
+setprop persist.radio.vilte_downgrade 1
+# drmserver is the last thing left of Digital Restrictions Management (DRM) in the ROM, which shall be taken care of by this Magisk module.
+watch -n 0.8 killall drmserver -v
